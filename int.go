6ac7125d647f40bf5b57d9affd0c4d71b96ec496
yaziciahmet <yaziciahmet@sabanciuniv.edu>
2023-11-25 20:32:32 +0100

    support more int methods

--- int.go
+++ int.go
@@ -54,6 +54,46 @@ func (i *BuzzInt) Max(max int) *BuzzInt {
 	return i
 }
 
+func (i *BuzzInt) Positive() *BuzzInt {
+	i.addValidateFunc(func(v int) error {
+		if v <= 0 {
+			return makeValidationError(i.name, "positive", "positive failed")
+		}
+		return nil
+	})
+	return i
+}
+
+func (i *BuzzInt) Nonnegative() *BuzzInt {
+	i.addValidateFunc(func(v int) error {
+		if v < 0 {
+			return makeValidationError(i.name, "nonnegative", "nonnegative failed")
+		}
+		return nil
+	})
+	return i
+}
+
+func (i *BuzzInt) Negative() *BuzzInt {
+	i.addValidateFunc(func(v int) error {
+		if v >= 0 {
+			return makeValidationError(i.name, "negative", "negative failed")
+		}
+		return nil
+	})
+	return i
+}
+
+func (i *BuzzInt) NonPositive() *BuzzInt {
+	i.addValidateFunc(func(v int) error {
+		if v > 0 {
+			return makeValidationError(i.name, "nonpositive", "nonpositive failed")
+		}
+		return nil
+	})
+	return i
+}
+
 func (i *BuzzInt) addValidateFunc(fn BuzzIntValidateFunc) {
 	i.validateFuncs = append(i.validateFuncs, fn)
 }
