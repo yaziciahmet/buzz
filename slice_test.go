16d70d59daf4ae2ddc8374c961a54fb5966ef736
yaziciahmet <yaziciahmet@sabanciuniv.edu>
2023-11-25 19:23:47 +0100

    add slice tests

--- slice_test.go
+++ slice_test.go
@@ -1,1 +1,42 @@
 package buzz
+
+import (
+	"errors"
+	"testing"
+)
+
+func Test_SliceForEachSuccess(t *testing.T) {
+	slice := []int{1, 2, 3}
+
+	if err := Slice[int]("").ForEach(func(v int) error {
+		if v == 5 {
+			return errors.New("you shall not pass")
+		}
+		return nil
+	}).Validate(slice); err != nil {
+		t.FailNow()
+	}
+}
+
+func Test_SliceForEachFail(t *testing.T) {
+	slice := []int{1, 2, 3}
+
+	if err := Slice[int]("").ForEach(func(v int) error {
+		if v == 3 {
+			return errors.New("you shall not pass")
+		}
+		return nil
+	}).Validate(slice); err == nil {
+		t.FailNow()
+	}
+}
+
+func Test_SliceForEachWithFieldValidationSuccess(t *testing.T) {
+	emails := []string{"ahmet@mail.com", "yazici@mail.com"}
+
+	if err := Slice[string]("").ForEach(func(v string) error {
+		return String("").Email().Validate(v)
+	}).Validate(emails); err != nil {
+		t.FailNow()
+	}
+}
