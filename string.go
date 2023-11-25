abf0fcf15105783fd7b32409cfc023e3dc97385a
yaziciahmet <yaziciahmet@sabanciuniv.edu>
2023-11-25 20:22:21 +0100

    support more string methods

--- string.go
+++ string.go
@@ -2,7 +2,10 @@ package buzz
 
 import (
 	"net/mail"
+	"net/url"
 	"reflect"
+	"regexp"
+	"strings"
 )
 
 var (
@@ -69,9 +72,65 @@ func (s *BuzzString) Len(l int) *BuzzString {
 
 func (s *BuzzString) Email() *BuzzString {
 	s.addValidateFunc(func(v string) error {
-		_, err := mail.ParseAddress(v)
-		if err != nil {
+		if _, err := mail.ParseAddress(v); err != nil {
 			return makeValidationError(s.name, "email", "email failed")
+		}
+		return nil
+	})
+	return s
+}
+
+func (s *BuzzString) URL() *BuzzString {
+	s.addValidateFunc(func(v string) error {
+		if _, err := url.ParseRequestURI(v); err != nil {
+			return makeValidationError(s.name, "url", "url failed")
+		}
+		return nil
+	})
+	return s
+}
+
+func (s *BuzzString) Regex(regex string) *BuzzString {
+	r, err := regexp.Compile(regex)
+	s.addValidateFunc(func(v string) error {
+		if err != nil {
+			return err
+		}
+
+		if !r.MatchString(v) {
+			return makeValidationError(s.name, "regex", "regex failed")
+		}
+
+		return nil
+	})
+	return s
+}
+
+func (s *BuzzString) Contains(str string) *BuzzString {
+	s.addValidateFunc(func(v string) error {
+		if !strings.Contains(v, str) {
+			return makeValidationError(s.name, "contains", "contains failed")
+		}
+		return nil
+	})
+	return s
+}
+
+func (s *BuzzString) StartsWith(str string) *BuzzString {
+	s.addValidateFunc(func(v string) error {
+		if !strings.HasPrefix(v, str) {
+
+			return makeValidationError(s.name, "startsWith", "startsWith failed")
+		}
+		return nil
+	})
+	return s
+}
+
+func (s *BuzzString) EndsWith(str string) *BuzzString {
+	s.addValidateFunc(func(v string) error {
+		if !strings.HasSuffix(v, str) {
+			return makeValidationError(s.name, "endsWith", "endsWith failed")
 		}
 		return nil
 	})
