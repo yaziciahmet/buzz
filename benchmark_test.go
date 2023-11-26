d06e7a9483058274d67b268bbf635a5bcf22b255
yaziciahmet <yaziciahmet@sabanciuniv.edu>
2023-11-26 02:21:18 +0100

    add parallel benchmark

--- benchmark_test.go
+++ benchmark_test.go
@@ -21,3 +21,25 @@ func Benchmark_SimpleStruct(b *testing.B) {
 		schema.Validate(user)
 	}
 }
+
+func Benchmark_SimpleStructParallel(b *testing.B) {
+	schema := Schema(
+		User{},
+		Field("Id", Int().Min(0).Max(1000)),
+		Field("Name", String().Min(2).Max(20)),
+		Field("Email", String().Email()),
+	)
+
+	user := User{
+		Id:    100,
+		Name:  "ahmet",
+		Email: "ahmet@gmail.com",
+	}
+
+	b.ResetTimer()
+	b.RunParallel(func(p *testing.PB) {
+		for p.Next() {
+			schema.Validate(user)
+		}
+	})
+}
