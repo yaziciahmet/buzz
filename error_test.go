654053e3ccdef8a74fd7bcb4f610fb9761511d5c
yaziciahmet <yaziciahmet@sabanciuniv.edu>
2023-11-26 20:57:01 +0100

    add Error type tests

--- error_test.go
+++ error_test.go
@@ -1,1 +1,15 @@
 package buzz
+
+import "testing"
+
+func Test_ErrorSimple(t *testing.T) {
+	if err := Error().MustBeType(&MyError{}).Validate(&MyError{}); err != nil {
+		t.FailNow()
+	}
+}
+
+func Test_ErrorNil(t *testing.T) {
+	if err := Error().Validate(nil); err != nil {
+		t.FailNow()
+	}
+}
