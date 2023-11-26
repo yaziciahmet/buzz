ca5bdc4a0d4925ba1f97577c384246c05355c225
yaziciahmet <yaziciahmet@sabanciuniv.edu>
2023-11-26 16:56:22 +0100

    add complex parallel benchmark

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -108,3 +108,66 @@ func Benchmark_ComplexStruct(b *testing.B) {
 		schema.Validate(complexStruct1)
 	}
 }
+
+func Benchmark_ComplexStructParallel(b *testing.B) {
+	now := time.Now()
+
+	schema := Schema(
+		ComplexStruct{},
+		Field("Id", Number[int]()),
+		Field("Email", String()),
+		Field("Spouse", Ptr(Schema(
+			User{},
+			Field("Id", Number[int]().Min(1)),
+			Field("Email", String().Email()),
+			Field("Name", String()),
+		))),
+		Field("Int64", Number[int64]()),
+		Field("Comments", Slice[string]().Max(10)),
+		Field("Friends", Slice[User]().ForEach(func(v User) error {
+			if v.Id > 100 {
+				return errors.New("too big id bro")
+			}
+			return nil
+		})),
+		Field("FriendsWithPtrUsers", Slice[*User]().Min(1)),
+		Field("CreatedAt", Time().After(now.Add(-1))),
+		Field("UpdatedAt", Ptr(Time())),
+		Field("LastError", Ptr(String()).Nullable()),
+	).Custom(func(cs ComplexStruct) error {
+		if cs.LastError != nil {
+			return errors.New(*cs.LastError)
+		}
+		return nil
+	})
+
+	complexStruct1 := ComplexStruct{
+		Id:    100,
+		Email: "ahmet@mail.com",
+		Spouse: &User{
+			Id:    1,
+			Email: "spouse@mail.com",
+			Name:  "spouse",
+		},
+		Int64:    241321,
+		Comments: []string{"no comment"},
+		Friends: []User{{
+			Id: 2,
+		}, {
+			Id: 99,
+		}},
+		FriendsWithPtrUsers: []*User{{
+			Id: 22,
+		}},
+		CreatedAt: now,
+		UpdatedAt: &now,
+		LastError: nil,
+	}
+
+	b.ResetTimer()
+	b.RunParallel(func(p *testing.PB) {
+		for p.Next() {
+			schema.Validate(complexStruct1)
+		}
+	})
+}
